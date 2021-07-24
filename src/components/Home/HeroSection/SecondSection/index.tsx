a7962289c250f240d8bf692084256fda6e617999
KiwiDev808 <lucasbacela@hotmail.com>
2021-07-24 15:59:23 -0300

    feat:second part of hero section

--- src/components/Home/HeroSection/SecondSection/index.tsx
+++ src/components/Home/HeroSection/SecondSection/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import background from '../../../../../public/images/HeroSection/second-section-background.jpg'
+import styles from './styles.module.scss'
 
 export const SecondSection = () => {
+  const backgroundUrl = background.src
   return (
-    <div>
-      <h3></h3>
-      <h2></h2>
+    <div
+      className={styles.sectionContainer}
+      style={{
+        backgroundImage: `linear-gradient(#00000051, #00000066), url(${backgroundUrl})`,
+      }}
+    >
+      <h3 className={styles.firstHeading}>Conheça-nos melhor</h3>
+      <h2 className={styles.secondHeading}>
+        Produtores Organicos Precisam de Você
+      </h2>
       <ul>
         <li>Centenas de Familias Amparadas</li>
         <li>Voluntariado Independente</li>
