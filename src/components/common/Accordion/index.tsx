a074ebc1137ed065ffdf3d5006f27d868cf6dd09
KiwiDev808 <lucasbacela@hotmail.com>
2021-07-25 21:36:20 -0300

    feat: added page perguntas frequentes

---
+++ src/components/common/Accordion/index.tsx
@@ -0,0 +1,28 @@
+import {
+  Accordion as MuiAccordion,
+  AccordionDetails,
+  AccordionSummary,
+  Typography,
+} from '@material-ui/core'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import React from 'react'
+
+const Accordion = (props) => {
+  const { title, description } = props
+  return (
+    <MuiAccordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography>{title}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Typography>{description}</Typography>
+      </AccordionDetails>
+    </MuiAccordion>
+  )
+}
+
+export default Accordion
