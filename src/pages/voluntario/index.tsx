a09518976667ce37b4882fdcdb11a9e5ae247478
Cleby Francisco <clebysilva5@gmail.com>
2021-07-25 23:21:21 -0300

    fix: volunteer section

--- src/pages/voluntario/index.tsx
+++ src/pages/voluntario/index.tsx
@@ -1,129 +1,132 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { useState } from 'react'
-import Layout from '../../components/common/Layout/Layout'
-import ModalVoluntario from './ModalVoluntario/ModalVoluntario'
-import styles from './styles.module.scss'
+import React, { useState } from "react";
+import Layout from "../../components/common/Layout/Layout";
+import ModalVoluntario from "./ModalVoluntario/ModalVoluntario";
+import styles from "./styles.module.scss";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
 
-const Voluntario = () => {
-  const [isModalVisible, SetIsModalVisible] = useState(false)
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+        width: "25ch",
+      },
+    },
+  })
+);
+
+const Voluntario = () => { 
+
+  const currencies = [
+    { value: "AC", label: "Acre" },
+    { value: "AL", label: "Alagoas" },
+    { value: "AP", label: "Amapá" },
+    { value: "AM", label: "Amazonas" },
+    { value: "BA", label: "Bahia" },
+    { value: "CE", label: "Ceará" },
+    { value: "DF", label: "Distrito Federal" },
+    { value: "ES", label: "Espirito Santo" },
+    { value: "GO", label: "Goiás" },
+    { value: "MA", label: "Maranhão" },
+    { value: "MS", label: "Mato Grosso do Sul" },
+    { value: "MT", label: "Mato Grosso" },
+    { value: "MG", label: "Minas Gerais" },
+    { value: "PA", label: "Pará" },
+    { value: "PB", label: "Paraíba" },
+    { value: "PR", label: "Paraná" },
+    { value: "PE", label: "Pernambuco" },
+    { value: "PI", label: "Piauí" },
+    { value: "RJ", label: "Rio de Janeiro" },
+    { value: "RN", label: "Rio Grande do Norte" },
+    { value: "RS", label: "Rio Grande do Sul" },
+    { value: "RO", label: "Rondônia" },
+    { value: "RR", label: "Roraima" },
+    { value: "SC", label: "Santa Catarina" },
+    { value: "SP", label: "São Paulo" },
+    { value: "SE", label: "Sergipe" },
+    { value: "TO", label: "Tocantins" },
+  ];
+
+
+  const [isModalVisible, SetIsModalVisible] = useState(false);
+  const classes = useStyles();
+  const [currency, setCurrency] = React.useState("SP");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrency(event.target.value);
+  };
   return (
     <Layout title="Voluntário - Sou Cesta Solidária">
-      <div className={styles.voluntario}>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <h2>O que é ser voluntário?</h2>
-          <p>
-            O voluntariado é sobre trocas - de experiência, conhecimento,
-            serviço, apoio, cidadania, carinho, amor - existindo um ganho não
-            apenas para a organização, mas também para o voluntário. É uma
-            experiência para todes - independe de gênero, raça ou etnia - que
-            permite conhecer novas realidades, se engajar em uma causa social,
-            contribuir com suas habilidades, e até mesmo desenvolver novas
-            competências
-          </p>
-          <button
-            className={styles.buttonVoluntario}
-            onClick={() => SetIsModalVisible(true)}
-          >
-            Confira a Lei do Voluntariado
-          </button>
-          {isModalVisible ? (
-            <ModalVoluntario onClose={() => SetIsModalVisible(false)}>
-              <h2>Lei do voluntariado</h2>
-            </ModalVoluntario>
-          ) : null}
-        </div>
-      </div>
-      <form className={styles.cadastro}>
-        <h2>Cadastrar-se como voluntário e ajude a mudar vidas!</h2>
-        <label htmlFor="name">Nome:</label>
-        <input id="name" type="text" />
-
-        <label htmlFor="email">E-mail:</label>
-        <input id="email" type="email" />
-        <div className={styles.cadline}>
-          <div>
-            <label htmlFor="tel">Tel/WhatsApp:</label>
-            <input id="tel" type="tel" />
-          </div>
-          <div>
-            <label htmlFor="date">Data de Nascimento:</label>
-            <input id="date" type="date" />
-          </div>
-        </div>
-
-        <div className={styles.cadline}>
-          <div>
-            <label htmlFor="cep">Cidade:</label>
-            <input id="cep" type="text" />
-          </div>
-          <div>
-            <label htmlFor="estado">Estado:</label>
-            <select id="estado">
-              <option value="SP">São Paulo</option>
-              <option value="AC">Acre</option>
-              <option value="AL">Alagoas</option>
-              <option value="AP">Amapá</option>
-              <option value="AM">Amazonas</option>
-              <option value="BA">Bahia</option>
-              <option value="CE">Ceará</option>
-              <option value="DF">Distrito Federal</option>
-              <option value="ES">Espírito Santo</option>
-              <option value="GO">Goiás</option>
-              <option value="MA">Maranhão</option>
-              <option value="MT">Mato Grosso</option>
-              <option value="MS">Mato Grosso do Sul</option>
-              <option value="MG">Minas Gerais</option>
-              <option value="PA">Pará</option>
-              <option value="PB">Paraíba</option>
-              <option value="PR">Paraná</option>
-              <option value="PE">Pernambuco</option>
-              <option value="PI">Piauí</option>
-              <option value="RJ">Rio de Janeiro</option>
-              <option value="RN">Rio Grande do Norte</option>
-              <option value="RS">Rio Grande do Sul</option>
-              <option value="RO">Rondônia</option>
-              <option value="RR">Roraima</option>
-              <option value="SC">Santa Catarina</option>
-              <option value="SE">Sergipe</option>
-              <option value="TO">Tocantins</option>
-            </select>
-          </div>
-          <div>
-            <label htmlFor="cep">Cep:</label>
-            <input id="cep" type="text" />
-          </div>
-        </div>
-        <div>
-          <label htmlFor="endereco">Endereço:</label>
-          <input id="endereco" type="text" />
-
-          <label htmlFor="numro">Numero:</label>
-          <input id="numro" type="text" />
-        </div>
-        <label htmlFor="endereco">Complemento:</label>
-        <input id="complemento" type="text" />
-        <div className={styles.cadhab}>
-          <label htmlFor="habilidades">Habilidade: </label>
-          <select name="" id="habilidades">
-            <option>Organização</option>
-            <option>Gerenciamento</option>
-            <option>Comunicação</option>
-            <option>Trabalho manual</option>
-            <option>Cozinha</option>
-            <option>Educação</option>
-            <option>Outros</option>
-          </select>
-        </div>
-
-        <button className={styles.btnEnviar} type="submit">
-          Enviar
+      <div className={styles.infoVoluntario}>
+        <h2>O que é ser voluntário?</h2>
+        <p>
+          O voluntariado é sobre trocas - de experiência, conhecimento, serviço,
+          apoio, cidadania, carinho, amor - existindo um ganho não apenas para a
+          organização, mas também para o voluntário. É uma experiência para
+          todes - independe de gênero, raça ou etnia - que permite conhecer
+          novas realidades, se engajar em uma causa social, contribuir com suas
+          habilidades, e até mesmo desenvolver novas competências
+        </p>
+        <button
+          className={styles.buttonVoluntario}
+          onClick={() => SetIsModalVisible(true)}
+        >
+          Confira a Lei do Voluntariado
         </button>
+        {isModalVisible ? (
+          <ModalVoluntario onClose={() => SetIsModalVisible(false)}>
+            <h2>Lei do voluntariado</h2>
+          </ModalVoluntario>
+        ) : null}
+      </div>
+      <form className={styles.formVoluntario} noValidate autoComplete="off">
+        <h2>Cadastrar-se como voluntário e ajude a mudar vidas!</h2>
+        <div>
+          <h3>Seja bem vindx!</h3>
+
+          <p>
+            A iniciativa social e sem fins lucrativos Sou Cesta Solidária tem
+            por objetivo nutrir com produtos orgânicos para trazer mais saúde
+            para todos, ao mesmo tempo em que fortalece o pequeno produtor e
+            famílias e grupos em situação de vulnerabilidade social, como
+            moradores de rua, através de doações de alimentos, vestuário e
+            outros insumos aos grupos que atendem esses grupos. Pedimos a
+            gentileza de que você, candidatx, preencha este Ficha de Interesse
+            com suas informações, e, caso identifiquemos compatibilidade do seu
+            perfil com o projeto, retornaremos o contato.
+          </p>
+        </div>
+        <TextField id="standard-basic" label="Nome:" type="text" />
+        <TextField id="standard-basic" label="E-mail:" type="email" />
+        <TextField id="standard-basic" label="Tel/WhatsApp:" type="tel" />
+        <TextField id="outlined-basic" type="date" />
+        <TextField id="standard-basic" label="Cidade:" type="text" />
+        <TextField
+          id="standard-select-currency"
+          select
+          label="Select"
+          value={currency}
+          onChange={handleChange}
+        >
+          {currencies.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+
+        <TextField id="standard-basic" label="Cep:" type="text" />
+        <TextField id="standard-basic" label="Endereço:" type="text" />
+        <TextField id="standard-basic" label="Numero:" type="text" />
+        
+      
+        <button>Enviar</button>
       </form>
     </Layout>
-  )
+  );
 }
 
-export default Voluntario
+export default Voluntario;
