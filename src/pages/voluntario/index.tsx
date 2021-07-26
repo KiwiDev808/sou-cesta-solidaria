5f5083b78531e2f6b961078b71a53b72a7f69001
KiwiDev808 <lucasbacela@hotmail.com>
2021-07-26 17:58:17 -0300

    style: fix form style of voluntario page

--- src/pages/voluntario/index.tsx
+++ src/pages/voluntario/index.tsx
@@ -3,6 +3,8 @@ import MenuItem from '@material-ui/core/MenuItem'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
 import React, { useState } from 'react'
+import { Button } from '../../components/common/Button'
+import Input from '../../components/common/Control/Input'
 import Layout from '../../components/common/Layout/Layout'
 import ModalVoluntario from './ModalVoluntario/ModalVoluntario'
 import styles from './styles.module.scss'
@@ -77,101 +79,102 @@ const Voluntario = () => {
   }
   return (
     <Layout title="Voluntário - Sou Cesta Solidária">
-      <div className={styles.infoVoluntario}>
-        <h2>O que é ser voluntário?</h2>
-        <p>
-          O voluntariado é sobre trocas - de experiência, conhecimento, serviço,
-          apoio, cidadania, carinho, amor - existindo um ganho não apenas para a
-          organização, mas também para o voluntário. É uma experiência para
-          todes - independe de gênero, raça ou etnia - que permite conhecer
-          novas realidades, se engajar em uma causa social, contribuir com suas
-          habilidades, e até mesmo desenvolver novas competências
-        </p>
-        <button
-          className={styles.buttonVoluntario}
-          onClick={() => SetIsModalVisible(true)}
-        >
-          Confira a Lei do Voluntariado
-        </button>
-        {isModalVisible ? (
-          <ModalVoluntario onClose={() => SetIsModalVisible(false)}>
-            <h2>Lei do voluntariado</h2>
-          </ModalVoluntario>
-        ) : null}
-      </div>
-      <form className={styles.formVoluntario} noValidate autoComplete="off">
-        <h2>Cadastrar-se como voluntário e ajude a mudar vidas!</h2>
-        <div>
-          <h3>Seja bem vindx!</h3>
-
+      <div className={styles.voluntarioContainer}>
+        <div className={styles.infoVoluntario}>
+          <h2>O que é ser voluntário?</h2>
           <p>
-            A iniciativa social e sem fins lucrativos Sou Cesta Solidária tem
-            por objetivo nutrir com produtos orgânicos para trazer mais saúde
-            para todos, ao mesmo tempo em que fortalece o pequeno produtor e
-            famílias e grupos em situação de vulnerabilidade social, como
-            moradores de rua, através de doações de alimentos, vestuário e
-            outros insumos aos grupos que atendem esses grupos. Pedimos a
-            gentileza de que você, candidatx, preencha este Ficha de Interesse
-            com suas informações, e, caso identifiquemos compatibilidade do seu
-            perfil com o projeto, retornaremos o contato.
+            O voluntariado é sobre trocas - de experiência, conhecimento,
+            serviço, apoio, cidadania, carinho, amor - existindo um ganho não
+            apenas para a organização, mas também para o voluntário. É uma
+            experiência para todes - independe de gênero, raça ou etnia - que
+            permite conhecer novas realidades, se engajar em uma causa social,
+            contribuir com suas habilidades, e até mesmo desenvolver novas
+            competências
           </p>
+          <button
+            className={styles.buttonVoluntario}
+            onClick={() => SetIsModalVisible(true)}
+          >
+            Confira a Lei do Voluntariado
+          </button>
+          {isModalVisible ? (
+            <ModalVoluntario onClose={() => SetIsModalVisible(false)}>
+              <h2>Lei do voluntariado</h2>
+            </ModalVoluntario>
+          ) : null}
         </div>
-        <TextField id="standard-basic" label="Nome:" type="text" />
-        <TextField id="standard-basic" label="E-mail:" type="email" />
-        <TextField id="standard-basic" label="Tel/WhatsApp:" type="tel" />
-        <label htmlFor="dataNascimento" className={styles.dataNascimento}>
-          Data de Nascimento:
-        </label>
-        <input
-          id="dataNascimento"
-          type="date"
-          className={styles.dataNascimentoInput}
-        />
-        {/* <TextField id="outlined-basic" type="date" /> */}
-        <TextField id="standard-basic" label="Cidade:" type="text" />
-        <TextField
-          id="standard-select-currency"
-          select
-          label="Estado:"
-          value={currency}
-          onChange={handleChange}
-        >
-          {estado.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-
-        <TextField id="standard-basic" label="Cep:" type="text" />
-        <TextField id="standard-basic" label="Endereço:" type="text" />
-        <TextField id="standard-basic" label="Numero:" type="text" />
-        <TextField
-          id="standard-select-currency"
-          select
-          label="Indique a área principal onde gostaria de atuar:"
-          value={currency}
-          onChange={handleChange}
-        >
-          {habilidades.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-        <TextField
-          id="outlined-basic"
-          label="Mensagem:"
-          type="textarea"
-          multiline
-          minRows={6}
-        />
-        {/* <Input label="Mensagem:" type="text" multiline minRows={6} /> */}
-
-        <button type="submit" className={styles.btnEnviar}>
-          Cadastrar
-        </button>
-      </form>
+        <form className={styles.formVoluntario} noValidate autoComplete="off">
+          <h2>Cadastrar-se como voluntário e ajude a mudar vidas!</h2>
+          <div>
+            <h3>Seja bem vindx!</h3>
+            <p>
+              A iniciativa social e sem fins lucrativos Sou Cesta Solidária tem
+              por objetivo nutrir com produtos orgânicos para trazer mais saúde
+              para todos, ao mesmo tempo em que fortalece o pequeno produtor e
+              famílias e grupos em situação de vulnerabilidade social, como
+              moradores de rua, através de doações de alimentos, vestuário e
+              outros insumos aos grupos que atendem esses grupos. Pedimos a
+              gentileza de que você, candidatx, preencha este Ficha de Interesse
+              com suas informações, e, caso identifiquemos compatibilidade do
+              seu perfil com o projeto, retornaremos o contato.
+            </p>
+          </div>
+          <div className={styles.formInputs}>
+            <Input label="Nome:" type="text" />
+            <Input label="E-mail:" type="email" />
+            <Input label="Tel/WhatsApp:" type="tel" />
+            <label htmlFor="dataNascimento" className={styles.dataNascimento}>
+              Data de Nascimento:
+            </label>
+            <input
+              id="dataNascimento"
+              type="date"
+              className={styles.dataNascimentoInput}
+            />
+            {/* <TextField id="outlined-basic" type="date" /> */}
+            <TextField id="standard-basic" label="Cidade:" type="text" />
+            <TextField
+              id="standard-select-currency"
+              select
+              label="Estado:"
+              value={currency}
+              onChange={handleChange}
+            >
+              {estado.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+            <Input label="Cep:" type="text" />
+            <Input label="Endereço:" type="text" />
+            <Input label="Numero:" type="text" />
+            <TextField
+              id="standard-select-currency"
+              select
+              label="Indique a área principal onde gostaria de atuar:"
+              value={currency}
+              onChange={handleChange}
+            >
+              {habilidades.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              id="outlined-basic"
+              label="Mensagem:"
+              type="textarea"
+              multiline
+              minRows={6}
+            />
+            <Button type="submit" className={styles.btnEnviar} size="large">
+              Cadastrar
+            </Button>
+          </div>
+        </form>
+      </div>
     </Layout>
   )
 }
