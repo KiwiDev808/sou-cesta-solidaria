5b4b01dde3552bb0699da87c6ba4a0d52eb153c5
KiwiDev808 <lucasbacela@hotmail.com>
2021-07-27 16:46:09 -0300

    feat: added netlify-cms-auth-provider

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -1,27 +1,3 @@
-import { randomBytes } from 'crypto'
-import { IncomingMessage, ServerResponse } from 'http'
-import { AuthorizationCode } from 'simple-oauth2'
-import { config } from '../../lib/config'
-import { scopes } from '../../lib/scopes'
+import { createVercelBeginHandler } from 'netlify-cms-oauth-provider-node'
 
-export const randomString = () => randomBytes(4).toString('hex')
-
-const auth = async (req: IncomingMessage, res: ServerResponse) => {
-  const { host } = req.headers
-  const url = new URL(`https://${host}/${req.url}`)
-  const urlParams = url.searchParams
-  const provider = urlParams.get('provider')
-
-  const client = new AuthorizationCode(config(provider))
-
-  const authorizationUri = client.authorizeURL({
-    redirect_uri: `https://${host}/callback?provider=${provider}`,
-    scope: scopes[provider],
-    state: randomString(),
-  })
-
-  res.writeHead(301, { Location: authorizationUri })
-  res.end()
-}
-
-export default auth
+module.exports = createVercelBeginHandler({}, { useEnv: true })
