a082d0386259b1d518dffb6841a996f4c6f350fd
KiwiDev808 <lucasbacela@hotmail.com>
2021-07-27 14:56:36 -0300

    feat: added frontmatter package

---
+++ src/pages/teste.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { attributes, react as Sample } from '../../content/home.md'
+import Layout from '../components/common/Layout/Layout'
+
+export default function Teste() {
+  let { title, cats } = attributes
+
+  return (
+    <Layout title="Sou Cesta Solidária">
+      <div>
+        <h1>{title}</h1>
+        <Sample />
+        <ul>
+          {cats.map((cat, k) => (
+            <li key={k}>
+              <h2>{cat.name}</h2>
+              <p>{cat.description}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Layout>
+  )
+}
