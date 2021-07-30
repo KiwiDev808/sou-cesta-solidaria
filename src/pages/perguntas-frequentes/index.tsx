4ddae0d99d709d4329e8aefd51943e039516ed26
KiwiDev808 <lucasbacela@hotmail.com>
2021-07-29 21:40:16 -0300

    feaat: added questions fetch functiion

--- src/pages/perguntas-frequentes/index.tsx
+++ src/pages/perguntas-frequentes/index.tsx
@@ -1,18 +1,14 @@
-import * as fs from 'fs'
-import matter from 'gray-matter'
-import yaml from 'js-yaml'
-import path from 'path'
 import React from 'react'
 import Accordion from '../../components/common/Accordion'
 import Layout from '../../components/common/Layout/Layout'
+import { fetchQuestionContent, QuestionContent } from '../../lib/questions'
 import styles from './styles.module.scss'
 
-export type QuestionContent = {
-  readonly title: string
-  readonly body: string
-}
-
-const PerguntasFrequentes = ({ questions }) => {
+const PerguntasFrequentes = ({
+  questions,
+}: {
+  questions: QuestionContent[]
+}) => {
   return (
     <Layout title="Perguntas Frequentes - Sou Cesta Solidária">
       <div className={styles.perguntasFrequentesContainer}>
@@ -32,33 +28,9 @@ const PerguntasFrequentes = ({ questions }) => {
 }
 
 export async function getServerSideProps(context) {
-  const questionsDirectory = path.join(process.cwd(), '/content/questions')
+  const questionsPath = process.cwd() + '/content/questions'
 
-  const fileNames = fs.readdirSync(questionsDirectory)
-  const allQuestionsData = fileNames
-    .filter((it) => it.endsWith('.md'))
-    .map((fileName) => {
-      const fullPath = path.join(questionsDirectory, fileName)
-      const fileContents = fs.readFileSync(fullPath, 'utf8')
-
-      const matterResult = matter(fileContents, {
-        engines: {
-          yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object,
-        },
-      })
-
-      const matterData = {
-        title: matterResult.data?.title,
-        body: matterResult.content,
-      } as {
-        title: string
-        body: string
-      }
-
-      return matterData
-    })
-
-  const questions = allQuestionsData
+  const questions = fetchQuestionContent(questionsPath)
   return {
     props: { questions: questions }, // will be passed to the page component as props
   }
