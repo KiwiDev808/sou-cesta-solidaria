260576a33f3e8d802a53e2c2261f1a2e10956665
KiwiDev808 <lucasbacela@hotmail.com>
2021-07-29 22:49:50 -0300

    refactor: removed unused dependencies

--- next.config.js
+++ next.config.js
@@ -1,20 +1,4 @@
-const Mode = require('frontmatter-markdown-loader/mode')
-
 module.exports = {
   pageExtensions: ['tsx'],
   reactStrictMode: true,
-  webpack: (cfg) => {
-    cfg.module.rules.push({
-      test: /\.md$/,
-      use: [
-        {
-          loader: 'frontmatter-markdown-loader',
-          options: {
-            mode: [Mode.REACT],
-          },
-        },
-      ],
-    })
-    return cfg
-  },
 }
