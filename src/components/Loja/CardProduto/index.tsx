590fd64316f25c1e167ed8f0345de2f49e2c3ecb
KiwiDev808 <lucasbacela@hotmail.com>
2021-08-06 09:12:24 -0300

    performance: added nullish operator to CardProduto

--- src/components/Loja/CardProduto/index.tsx
+++ src/components/Loja/CardProduto/index.tsx
@@ -7,24 +7,24 @@ export const CardProduto = (props: ProductContent) => {
   return (
     <div className={styles.cardContainer}>
       <div className={styles.imageWrapper}>
-        <img src={props.image} alt="product photo" />
+        <img src={props?.image} alt="product photo" />
       </div>
       <div className={styles.description}>
         <h3>{props.title}</h3>
-        <h4>{props.basket_size}</h4>
-        <h5>{props.basket_type}</h5>
-        <h5>{props.invoice_type}</h5>
+        <h4>{props?.basket_size}</h4>
+        <h5>{props?.basket_type}</h5>
+        <h5>{props?.invoice_type}</h5>
       </div>
       <div className={styles.price}>
         <p>Cada cesta</p>
         <h4>
-          R$ <span>{props.price}</span>
+          R$ <span>{props?.price}</span>
         </h4>
       </div>
       <button
         className={styles.buyButton}
         onClick={() => {
-          window.open(props.url, 'newwindow', 'width=1000,height=600')
+          window.open(props?.url, 'newwindow', 'width=1000,height=600')
           return false
         }}
       >
